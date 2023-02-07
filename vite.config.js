8c227e1e438ce388f5067b15182eb4e40f450984
hohin <sungsill150@gmail.com>
2023-02-07 15:15:45 +0900

    123
    aasdasdasdas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'url';
 
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return defineConfig({
+    plugins: [
+      vue({
+      }),
+    ],
+    server: {
+      hmr: {
+        clientPort: env.CLIENT_PORT,
+      },
+      proxy: {
+        '/api': {
+          target:
+              env.APP_BACKEND == null ? 'http://localhost:8080' : env.APP_BACKEND,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};
